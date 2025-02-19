3838a06920cfd5bd2da41165bb3ea9348dac7844
Aspect <41125780+Aspect12@users.noreply.github.com>
2025-02-19 18:42:20 +0200

    Add music files.

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -1,6 +1,8 @@
 
 -- save player intro info
 -- decorate note creation popup
+-- prevent creating notes too close to each other
+-- make notes shared between servers
 
 AddCSLuaFile("cl_init.lua")
 AddCSLuaFile("shared.lua")
