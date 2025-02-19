6ef137841e2cd64ef0de90b5fcf96b2363ed5ef4
Aspect <41125780+Aspect12@users.noreply.github.com>
2025-02-19 18:33:56 +0200

    Play ambient music.

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -1,6 +1,5 @@
 
 -- save player intro info
--- play ambient music
 -- decorate note creation popup
 
 AddCSLuaFile("cl_init.lua")
--- gamemode/modules/cl_music.lua
+++ gamemode/modules/cl_music.lua
@@ -1,2 +1,46 @@
 
 -- Plays ambient music
+local songs = {
+	"echoesbeyond/music/eo_all_around us.mp3",
+	"echoesbeyond/music/eo_a_sunset.mp3",
+	"echoesbeyond/music/eo_below_the clouds.mp3",
+	"echoesbeyond/music/eo_dead_metal.mp3",
+	"echoesbeyond/music/eo_it's_expanding.mp3",
+	"echoesbeyond/music/eo_monolith.mp3",
+	"echoesbeyond/music/eo_red_ocean.mp3",
+	"echoesbeyond/music/eo_reflect.mp3",
+	"echoesbeyond/music/eo_reverse.mp3",
+	"echoesbeyond/music/eo_strange_flyer.mp3",
+	"echoesbeyond/music/eo_the_heights.mp3",
+	"echoesbeyond/music/eo_the_oddity.mp3",
+	"echoesbeyond/music/eo_yesterday.mp3",
+	"echoesbeyond/music/eo_yesterdays.mp3"
+}
+
+local currentSong
+
+local function PlayMusic()
+	local soundPath = songs[math.random(#songs)]
+	local soundDuration = SoundDuration(soundPath) + 10
+
+	timer.Create("echoes_music", soundDuration, 1, function()
+		PlayMusic()
+	end)
+
+	sound.PlayFile("sound/" .. soundPath, "mono", function(station)
+		if (!IsValid(station)) then return end
+
+		if (IsValid(currentSong)) then
+			currentSong:Stop()
+		end
+
+		station:SetVolume(0.5)
+		station:Play()
+
+		currentSong = station
+	end)
+end
+
+hook.Add("InitPostEntity", "music_InitPostEntity", function()
+	PlayMusic()
+end)
