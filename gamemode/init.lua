96aa931a6e71723344589cadd15f232dbac188b4
Aspect <41125780+Aspect12@users.noreply.github.com>
2025-02-19 19:49:27 +0200

    Add intro.

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -1,8 +1,12 @@
 
--- save player intro info
 -- decorate note creation popup
+-- create notification system
 -- prevent creating notes too close to each other
+-- prevent creating notes too close to spawnpoints
 -- make notes shared between servers
+-- prevent hateful comments (profanity filter, use regex or something?)
+	-- or maybe mark them as red and have a tab menu that allows you to show them? could be cooler
+		-- auto-enable the profanity option if a player writes something with profanity
 
 AddCSLuaFile("cl_init.lua")
 AddCSLuaFile("shared.lua")
---
+++ gamemode/modules/cl_intro.lua
@@ -0,0 +1,72 @@
+
+-- Small intro sequence
+hook.Add("InitPostEntity", "intro_InitPostEntity", function()
+	if (file.Exists("echoesbeyond/expirednotes.txt", "DATA")) then return end
+
+	local intro = vgui.Create("DPanel")
+	intro:SetSize(ScrW(), ScrH())
+	intro:MakePopup()
+	intro:Center()
+	intro:SetCursor("blank")
+	intro.Paint = function(self, width, height)
+		surface.SetDrawColor(25, 25, 25)
+		surface.DrawRect(0, 0, width, height)
+	end
+
+	timer.Simple(3, function()
+		local text = vgui.Create("DLabel", intro)
+		text:SetFont("DermaLarge")
+		text:SetText("This game is connected to the internet, and all echoes are shared across all players.")
+		text:SizeToContents()
+		text:Center()
+		text:SetAlpha(0)
+		text:AlphaTo(255, 0.5)
+
+		timer.Simple(5, function()
+			text:AlphaTo(0, 0.5, 0, function()
+				text:Remove()
+			end)
+
+			timer.Simple(2, function()
+				text = vgui.Create("DLabel", intro)
+				text:SetFont("DermaLarge")
+				text:SetText("Please be respectful and considerate of others when echoing.")
+				text:SizeToContents()
+				text:Center()
+				text:SetAlpha(0)
+				text:AlphaTo(255, 0.5)
+
+				timer.Simple(5, function()
+					text:AlphaTo(0, 0.5, 0, function()
+						text:Remove()
+					end)
+
+					timer.Simple(2, function()
+						text = vgui.Create("DLabel", intro)
+						text:SetFont("DermaLarge")
+						text:SetText("Press TAB for options, and R to echo.")
+						text:SizeToContents()
+						text:Center()
+						text:SetAlpha(0)
+						text:AlphaTo(255, 0.5)
+
+						timer.Simple(5, function()
+							text:AlphaTo(0, 0.5, 0, function()
+								text:Remove()
+							end)
+
+							timer.Simple(1, function()
+								intro:SetKeyboardInputEnabled(false)
+								intro:SetMouseInputEnabled(false)
+
+								intro:AlphaTo(0, 5, 0, function()
+									intro:Remove()
+								end)
+							end)
+						end)
+					end)
+				end)
+			end)
+		end)
+	end)
+end)
